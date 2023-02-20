adebded64c56bec979a161a1cd575d55d21444bc
nicolas-rigal <115165951+nicolas-rigal@users.noreply.github.com>
2023-02-20 16:17:16 +0100

    dernier version
    projet horloge 1/2 du aller plus loin valider

--- horloge.py
+++ horloge.py
@@ -1,74 +1,56 @@
 import time
 
-# https://www.pierre-giraud.com/python-apprendre-programmer-cours/module-datetime-time-calendar/#:~:text=La%20fonction%20time()%20renvoie,00%3A00%20(UTC). 
-#----------------------------------------------------------#
-#       initialisation de mes variable de parametre        #
-#                recuperation nom du user                  #
-#        et fonction de verification format 12/24          #
-#----------------------------------------------------------#
-#-%H	Heure (horloge sur 24 heures) sous forme de nombre décimal [00,23].
-#-%M	Minutes sous forme décimale [00,59].
-#-%S	Deuxième sous forme de nombre décimal [00,61].
-
-minute = int(time.strftime("%M"))
-second = int(time.strftime("%S"))
-# permet de vérifier si l'alarme a été initialisée ou non
-alarm_heure = -1 
-alarm_minute = -1
-alarm_second = -1.0
-alarm_on = False
-format_time = "24"
-
-# input de initialisation de l'heure, minute, seconde
 def afficher_heure():
     global heure, minute, second
-    heure = int(input("Voyez pararametre une heure pour initialiser votre horloge  :")) 
-    minute = int(input("Voyez pararametre une minutes pour initialiser votre horloge : "))
-    second = int(input("Voyez pararametre une second pour initialiser votre horloge : "))
-    
-# input de initialisation de l'heure, minute, seconde pour l'alarme 
-def set_alarm():
-    global alarm_heure, alarm_minute, alarm_second, alarm_on
-    alarm_heure = int(input("Voyez pararametre une heure pour votre alarme :"))
-    alarm_minute = int(input("Voyez pararametre une minutes pour votre alarme :"))
-    alarm_second = int(input("Voyez pararametre une second pour votre alarme :"))
-    alarm_on = True
+    heure = int(input("Veuillez entrer l'heure pour initialiser votre horloge :"))
+    minute = int(input("Veuillez entrer les minutes pour initialiser votre horloge : "))
+    second = int(input("Veuillez entrer les secondes pour initialiser votre horloge : "))
 
-# fonctiond deselection du format de l'heure
-def set_format_time():
-    global format_time
-    format_time = input("Quel format souhaitez vous ? (12 or 24) : ")
-    if format_time != "12" and format_time != "24":
-        format_time = input("Quel format souhaitez vous ? (12 or 24) : ")
-    
+def activation_alarm():
+    global alarm_on
+    alarm_on = input("Voulez-vous activer une alarme ? (oui/non) : ")
+    if alarm_on.lower() == "oui":
+        alarm_on = True
+    else :
+        alarm_on = False
+        
+def alarme_setting():
+    if alarm_on == True:
+        global alarm_heure, alarm_minute, alarm_second
+        alarm_heure = int(input("Veuillez entrer l'heure pour votre alarme :"))
+        alarm_minute = int(input("Veuillez entrer les minutes pour votre alarme :"))
+        alarm_second = int(input("Veuillez entrer les secondes pour votre alarme :"))
 
+def choisir_mode():
+    global mode_24h
+    mode_24h = input("Veuillez choisir le mode d'affichage de l'heure  (12/24) : ")
+    if mode_24h == "12":
+        mode_24h = False
+    else:
+        mode_24h = True
 
-# appele de mes fonction input avec user 
+# initialisation de l'heure 
 afficher_heure()
-set_alarm()
-set_format_time()
-
-
-#----------------------------------------------------------#
-#               initialisation de ma boucle                #
-#         verification du format si 12 selectionner        #
-#      fonctionement analogique d'un horloge clasique      #
-#      fonctionement analogique d'un horloge clasique      #
-#----------------------------------------------------------#
+# activation de l'alarme 
+activation_alarm()
+# initialisation de l'alarme 
+alarme_setting()
+# choix du mode d'affichage de l'heure
+choisir_mode()
 
 while True:
-    if format_time == "12" and heure < 12:
-        if heure == 0:
-            heure = 12
-#\r permet d'actualiser l'horloge
-            print("\r%02d:%02d:%02d AM" % (heure, minute, second), end="")
-        elif format_time == "12" and heure > 12:
-            heure -= 12
-            print("\r%02d:%02d:%02d PM" % (heure, minute, second), end="")
-    if format_time == "24":
+    if alarm_on == True:
+        if heure == alarm_heure and minute == alarm_minute and second == alarm_second:
+            print("\nJesse wake up! I don't like this! Jesse Wake up! Hello 👨‍🦱🤘🎸🎲")
+    if mode_24h:
         print("\r%02d:%02d:%02d" % (heure, minute, second), end="")
-    print("\r%02d:%02d:%02d" % (heure, minute, second), end="")
-
+    else:
+        if heure < 12:
+            print("\r%02d:%02d:%02d AM" % (heure, minute, second), end="")
+        elif heure == 12:
+            print("\r%02d:%02d:%02d PM" % (heure, minute, second), end="")
+        else:
+            print("\r%02d:%02d:%02d PM" % (heure-12, minute, second), end="")
     time.sleep(1)
     second += 1
     # fonctionnement d'une horloge classique 
@@ -79,14 +61,4 @@ while True:
         minute = 0
         heure += 1
     if heure == 24:
-        heure = 0
-    if alarm_on:
-        # permet de vérifier les conditions qui vont permettre à l'alarme de s'activer
-        if heure == alarm_heure or heure == alarm_heure - 12 and minute == alarm_minute and second == alarm_second:
-            print("\nJesse wake up ! I don't like this! Jesse Wake up! Hello 👨‍🦱🤘🎸🎲")
-            alarm_on = False
-            if alarm_heure > 12:
-                print("\r%02d:%02d:%02d PM" % (heure, minute, second), end="")
-            else:
-                print("\r%02d:%02d:%02d AM" % (heure, minute, second), end="")
-
+        heure = 00
